fb94aa1472db10878e2b78c8966a9068fa6ac9c1
pablo <pjcalvov@gmail.com>
2021-02-05 10:06:26 -0600

    fix to commit

--- src/main/java/specifications/RequestSpecs.java
+++ src/main/java/specifications/RequestSpecs.java
@@ -17,7 +17,7 @@ public class RequestSpecs {
 
     public static RequestSpecification generateFakeToken(){
         RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
-        requestSpecBuilder.addHeader("Authorization", "Bearer eyJhbGcaOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NfdXVpZCI6ImE4MDVkNjIwLWFmNzItNGQyMy04NTU1LTkxOTJkYTQ0NDdmNSIsImF1dGhvcml6ZWQiOnRydWUsImV4cCI6MTYxMjQ4NjEyMSwidXNlcl9pZCI6ODl9.2EgPPzWH2vOtLKr-FVkwmu0SsdI757yyONiHX6O7lF8");
+        requestSpecBuilder.addHeader("Authorization", "Beasadrer wrongtoken");
         return requestSpecBuilder.build();
     };
 
